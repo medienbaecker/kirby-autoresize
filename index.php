8239db2559916cfb0fe6fea440aeb97a104e36aa
Thomas Günther <mail@medienbaecker.com>
2019-01-11 14:12:11 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+Kirby::plugin('medienbaecker/autoresize', [
+    'options' => [
+        'maxWidth' => 2000
+    ],
+    'hooks' => [
+        'file.create:after' => function ($file) {
+            if($file->isResizable()) {
+                if($file->width() > option('medienbaecker.autoresize.maxWidth')) {
+                    try {
+                        kirby()->thumb($file->root(), $file->root(), [
+                            'width' => option('medienbaecker.autoresize.maxWidth')
+                        ]);
+                    } catch (Exception $e) {
+                        throw new Exception($e->getMessage());
+                    }
+                }
+            }
+        }
+    ]
+]);
